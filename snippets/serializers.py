2a1f6441c05a5963e0066af1294b4a28518fcfe4
neyvis <neyvis.remon.gonzalez@gmail.com>
2020-06-30 14:12:43 -0400

    Creating an endpoint for the highlighted snippets

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,20 +3,22 @@ from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
 
     def create(self, validated_data):
         """
@@ -35,3 +37,8 @@ class SnippetSerializer(serializers.ModelSerializer):
         instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
+
+
+
+
+
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import UserList, UserDetail, SnippetList, SnippetDetail
+from .views import UserList, UserDetail, SnippetList, SnippetDetail, api_root, SnippetHighlight
 
+
+# API endpoints
 urlpatterns = [
-    path('snippets/', SnippetList.as_view()),
-    path('snippets/<int:pk>/', SnippetDetail.as_view()),
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>/', UserDetail.as_view()),
+    path('', api_root),
+    path('snippets/', SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', SnippetDetail.as_view(),  name='snippet-detail'),
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+    path('snippets/<int:pk>/highlight/', SnippetHighlight.as_view(), name='snippet-highlight'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
